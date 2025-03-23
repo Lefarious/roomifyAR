ef68a6ba69ff16183da829f67318e40b5a72228f
Thisanda-Haththalla <138689000+Thisanda-Haththalla@users.noreply.github.com>
2025-03-23 22:30:38 +0530

    get crud for manufacturermodel

--- controllers/manufacturerModel.js
+++ controllers/manufacturerModel.js
@@ -1,6 +1,16 @@
 const asyncHandler = require('express-async-handler');
 const ManufacturerModel = require('../models/manufacturerModel');
 
+//@desc Get ManufacturerModel
+//@route GET /api/ManufacturerModels/:id
+//@access public
 
-
-module.exports = {getManufacturerModel, getManufacturerModels, createManufacturerModel, updateManufacturerModel, deleteManufacturerModel};
+const getManufacturerModel = asyncHandler (async (req,res,next) => {
+    const manufacturerModel = await ManufacturerModel.findById(req.params.id);
+    if (!manufacturerModel) {
+        res.status(404);
+        return next(new Error("ManufacturerModel not found"));
+    }
+    res.status(200).json(manufacturerModel);
+}
+);
