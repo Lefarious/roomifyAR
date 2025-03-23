d6f5d26694b4ea680ac7d2181a789c8e218dfb2a
Malin J <malin.20231247@iit.ac.lk>
2025-03-23 22:06:57 +0530

    Added CRUD for indieFurnitureModel

--- controllers/indieFurnitureModel.js
+++ controllers/indieFurnitureModel.js
@@ -19,6 +19,58 @@ const getIndieFurnitureModel = asyncHandler (async (req,res,next) => {
 //@route GET /api/IndieFurnitureModels/
 //@access public
 
+const getIndieFurnitureModels = asyncHandler (async (req,res) => {
+    const indieFurnitureModels = await IndieFurnitureModel.find();
+    res.status(200).json(indieFurnitureModels);
+}
+);
 
+//@desc Create new IndieFurnitureModel
+//@route POST /api/IndieFurnitureModels/
+//@access public
+
+const createIndieFurnitureModel = asyncHandler (async (req,res,next) => {
+    console.log("Request body : ", req.body);
+    const {name, userId} = req.body;
+    if (!name || !userId){
+        res.status(400);
+        return next(new Error("All fields are mandatory"));
+    }
+    const indieFurnitureModel = await IndieFurnitureModel.create({ name, userId });
+    res.status(201).json(indieFurnitureModel);
+}
+);
+
+//@desc Update IndieFurnitureModel
+//@route PUT /api/IndieFurnitureModels/:id
+//@access public
+
+const updateIndieFurnitureModel = asyncHandler (async (req,res,next) => {
+    const indieFurnitureModel = await IndieFurnitureModel.findById(req.params.id);
+    if (!indieFurnitureModel) {
+        res.status(404);
+        return next(new Error("IndieFurnitureModel not found"));
+    }
+    console.log("Request body : ", req.body);
+    const updatedIndieFurnitureModel = await IndieFurnitureModel.findByIdAndUpdate(req.params.id,req.body,{new: true});
+    res.status(201).json(updatedIndieFurnitureModel);
+   // res.status(200).json({message : `Update IndieFurnitureModels for ${req.params.id}`});
+}
+);
+
+//@desc Delete IndieFurnitureModel
+//@route DELETE /api/IndieFurnitureModels/:id
+//@access public
+
+const deleteIndieFurnitureModel = asyncHandler (async (req,res,next) => {
+    const indieFurnitureModel = await IndieFurnitureModel.findById(req.params.id);
+    if (!indieFurnitureModel) {
+        res.status(404);
+        return next(new Error("IndieFurnitureModel not found"));
+    }
+    await indieFurnitureModel.remove();
+    res.status(200).json({message : `Delete IndieFurnitureModel for ${req.params.id}`});
+}
+);
 
 module.exports = {getIndieFurnitureModel, getIndieFurnitureModels, createIndieFurnitureModel, updateIndieFurnitureModel, deleteIndieFurnitureModel};
